63db8fcabe78f3e04023f7c902d9de3858a9c9d0
guchaolong <guclno1@qq.com>
2020-09-09 14:55:23 +0800

    first commit

---
+++ src/main/java/com/zeki/java_project_stack/JavaProjectStackApplication.java
@@ -0,0 +1,13 @@
+package com.zeki.java_project_stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjectStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeki/java_project_stack/JavaProjectStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeki.java_project_stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
